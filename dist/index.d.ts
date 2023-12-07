d51c351c192b2e6f1466d7731c56283308515791
iiCoding <297854770@qq.com>
2023-12-07 11:45:56 +0800

    导出 storage hooks

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,3 +2,8 @@ import useMemoizedHandle from './hooks/useMemoizedHandle';
 import useRequest from './hooks/useRequest';
 import useRequestAll from './hooks/useRequestAll';
 export { useRequest, useRequestAll, useMemoizedHandle };
+/********************************************* useStorageChange ***********************************************/
+export * from './hooks/use-storage-change';
+export * from './hooks/use-storage-change/type';
+export { default as useStorageChange } from './hooks/use-storage-change';
+/********************************************* useStorageChange ***********************************************/
